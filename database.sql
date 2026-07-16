9995ab382b4b6d397613dce4ec9714111a313775
DefarelLabs <defarel06@gmail.com>
2026-07-16 14:42:40 +0700

    update generation time in SQL dump and ensure database creation statement is present

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 16, 2026 at 09:39 AM
+-- Generation Time: Jul 16, 2026 at 09:41 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_kasir_laundry`
 --
+CREATE DATABASE IF NOT EXISTS `db_kasir_laundry` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `db_kasir_laundry`;
 
 -- --------------------------------------------------------
 
